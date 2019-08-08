e5f235ca98ea042150eb4730dc001ec38709f403
JeanExtreme002 <jeangamerextreme@gmail.com>
2019-08-07 22:31:46 -0300

    corrected attribute names

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ def setConfig():
                     continue
                 
                 if "color" in key.lower():
+                    
                     if "colors" in key.lower():
                         colors_list = []
                     
@@ -94,7 +95,7 @@ def setConfig():
         # Passa os valores do dicionário para a classe principal do programa como atributo
         for mainKey in file_config.keys():
             for key in file_config[mainKey].keys():
-                if 'color' in key.lower(): print(file_config[mainKey])
+                if 'color' in key.lower():
                 setattr(App,key.upper(),file_config[mainKey][key])
                     
     except:
